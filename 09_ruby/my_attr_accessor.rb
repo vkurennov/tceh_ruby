d6dca2c03ddb35ca9c4814b236f0a380580cab9e
Vitaly Kurennov <vkurennov@gmail.com>
2015-10-15 22:10:10 +0300

    attr_accessor_with_history

--- 09_ruby/my_attr_accessor.rb
+++ 09_ruby/my_attr_accessor.rb
@@ -1,9 +1,21 @@
 module MyAttrAccessor
-  def my_attr_accessor(*names)
+  def attr_accessor_with_history(*names)
     names.each do |name|
       var_name = "@#{name}".to_sym
       define_method(name) { instance_variable_get(var_name) }
-      define_method("#{name}=") do |value| 
+
+      define_method("#{name}_history") do        
+        instance_variable_get("@#{name}_history")
+      end
+      
+      define_method("#{name}=") do |value|
+        history = instance_variable_get("@#{name}_history")
+        instance_variable_set("@#{name}_history", []) unless history 
+
+        current_val = instance_variable_get(var_name)
+        history = instance_variable_get("@#{name}_history")
+        history << current_val
+
         instance_variable_set(var_name, value)
       end
     end
@@ -13,5 +25,5 @@ end
 class Test
   extend MyAttrAccessor
 
-  my_attr_accessor :test, :test2, :test3
+  attr_accessor_with_history :test, :test2, :test3
 end
